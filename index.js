3516ac98bdb0bee6e24439094da565d47738a91e
Samuel fernandes <samuelfernandes10001@gamil.com>
2023-12-19 15:55:58 +0530

    4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get("/", (req, res)=>{
+app.get("/api", (req, res)=>{
  const intro = {
     name: "Samuel fernandes"
  }
